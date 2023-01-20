c07aa2866785c9ee03d0295d1e13fa0d0685eed1
Sina Rabiei <sina.rabiei@sectoralarm.com>
2023-01-20 12:34:16 +0100

    Add available platforms

--- Sources/NSAsyncCachedImage/NSAsyncCachedImage.swift
+++ Sources/NSAsyncCachedImage/NSAsyncCachedImage.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct NSAsyncCachedImage<Placeholder>: View where Placeholder: View {
 
     private let placeHolder: Placeholder
